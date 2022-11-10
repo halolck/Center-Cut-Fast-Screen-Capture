2aeca6f6629af3e5707fc086f97afbe6015a2448
halolck <muro02056624@gmail.com>
2022-11-10 17:09:47 +0900

    first
    first

---
+++ FastCutCapture/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FastCutCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(431, 274);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(449, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Height";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(449, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Width";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(449, 113);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(77, 19);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(449, 60);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(77, 19);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(449, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 300);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FastCutCapture/Form1.cs
@@ -0,0 +1,203 @@
+﻿using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastCutCapture
+{
+    public partial class Form1 : Form
+    {
+        int h = Screen.PrimaryScreen.Bounds.Height;
+        int w = Screen.PrimaryScreen.Bounds.Width;
+        public Rectangle rect;
+        ScreenStateLogger screenStateLogger = new ScreenStateLogger();
+        int i = 0;
+        bool captureflag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDown1.Maximum = w;
+            numericUpDown2.Maximum = h;
+            screenStateLogger.ChangeSize(new Rectangle(w / 2 - (int)numericUpDown1.Value / 2, h / 2 - (int)numericUpDown2.Value / 2, (int)numericUpDown1.Value, (int)numericUpDown2.Value));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (captureflag)
+            {
+                screenStateLogger.Stop();
+                pictureBox1.Image = null;
+            }
+            else
+            {
+                capturetest();
+            }
+            captureflag = !captureflag;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            screenStateLogger.ChangeSize(new Rectangle(w / 2 - (int)numericUpDown1.Value / 2, h / 2 - (int)numericUpDown2.Value / 2, (int)numericUpDown1.Value, (int)numericUpDown2.Value));
+
+        }
+
+        private void capturetest()
+        {
+            screenStateLogger.ScreenRefreshed += (sender, data) =>
+            {
+                //New frame in dataMemoryStream ms = new MemoryStream(img);
+                MemoryStream ms = new MemoryStream(data);
+
+                Bitmap bibit = new Bitmap(ms);
+                pictureBox1.Image = bibit;
+                ms.Close();
+                i++;
+            };
+            screenStateLogger.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Capture FPS: " + i.ToString();
+            i = 0;
+        }
+    }
+    public class ScreenStateLogger
+    {
+        private bool _run, _init;
+        private Rectangle rects = new Rectangle(0, 0, 100, 100);
+
+        public ScreenStateLogger()
+        {
+
+        }
+
+        public void ChangeSize(Rectangle rec)
+        {
+            rects = rec;
+        }
+
+        public void Start()
+        {
+            _run = true;
+            var factory = new Factory1();
+            //Get first adapter
+            var adapter = factory.GetAdapter1(0);
+            //Get device from adapter
+            var device = new SharpDX.Direct3D11.Device(adapter);
+            //Get front buffer of the adapter
+            var output = adapter.GetOutput(0);
+            var output1 = output.QueryInterface<Output1>();
+
+            // Width/Height of desktop to capture
+            int width = output.Description.DesktopBounds.Right;
+            int height = output.Description.DesktopBounds.Bottom;
+
+            // Create Staging texture CPU-accessible
+            var textureDesc = new Texture2DDescription //ここ直したらもっと早くなりそう
+            {
+                CpuAccessFlags = CpuAccessFlags.Read,
+                BindFlags = BindFlags.None,
+                Format = Format.B8G8R8A8_UNorm,
+                Width = width,
+                Height = height,
+                OptionFlags = ResourceOptionFlags.None,
+                MipLevels = 1,
+                ArraySize = 1,
+                SampleDescription = { Count = 1, Quality = 0 },
+                Usage = ResourceUsage.Staging
+            };
+            var screenTexture = new Texture2D(device, textureDesc);
+
+            Task.Factory.StartNew(() =>
+            {
+                // Duplicate the output
+                using (var duplicatedOutput = output1.DuplicateOutput(device))
+                {
+                    while (_run)
+                    {
+                        try
+                        {
+                            Rectangle rect = rects;
+                            SharpDX.DXGI.Resource screenResource;
+                            OutputDuplicateFrameInformation duplicateFrameInformation;
+
+                            // Try to get duplicated frame within given time is ms
+                            duplicatedOutput.AcquireNextFrame(5, out duplicateFrameInformation, out screenResource);
+
+                            // copy resource into memory that can be accessed by the CPU
+                            using (var screenTexture2D = screenResource.QueryInterface<Texture2D>())
+                                device.ImmediateContext.CopyResource(screenTexture2D, screenTexture);
+
+                            // Get the desktop capture texture
+                            var mapSource = device.ImmediateContext.MapSubresource(screenTexture, 0, MapMode.Read, SharpDX.Direct3D11.MapFlags.None);
+
+                            // Create Drawing.Bitmap
+                            using (var bitmap = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb))
+                            {
+                                // Copy pixels from screen capture Texture to GDI bitmap
+                                var mapDest = bitmap.LockBits(new Rectangle(0, 0, rect.Width, rect.Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                                if (1 + 1 == 2)
+                                {
+                                    var sourcePtr = mapSource.DataPointer + (rect.Left - 1) * 4 + width * (rect.Top - 1) * 4; //width /半分問題
+                                    var destPtr = mapDest.Scan0;
+                                    for (int y = 0; y < rect.Height; y++)
+                                    {
+                                        // Copy a single line 
+                                        Utilities.CopyMemory(destPtr, sourcePtr, rect.Width * 4);
+
+                                        // Advance pointers
+                                        sourcePtr = IntPtr.Add(sourcePtr, mapSource.RowPitch);
+                                        destPtr = IntPtr.Add(destPtr, mapDest.Stride);
+                                    }
+
+                                    // Release source and dest locks
+                                    bitmap.UnlockBits(mapDest);
+                                    device.ImmediateContext.UnmapSubresource(screenTexture, 0);
+
+                                    using (var ms = new MemoryStream())
+                                    {
+                                        bitmap.Save(ms, ImageFormat.Bmp);
+                                        ScreenRefreshed?.Invoke(this, ms.ToArray());
+                                        _init = true;
+                                    }
+                                }
+
+                            }
+                            screenResource.Dispose();
+                            duplicatedOutput.ReleaseFrame();
+                        }
+                        catch (SharpDXException e)
+                        {
+                            if (e.ResultCode.Code != SharpDX.DXGI.ResultCode.WaitTimeout.Result.Code)
+                            {
+                                Trace.TraceError(e.Message);
+                                Trace.TraceError(e.StackTrace);
+                            }
+                        }
+                    }
+                }
+            });
+            while (!_init) ;
+        }
+
+        public void Stop()
+        {
+            _run = false;
+        }
+
+        public EventHandler<byte[]> ScreenRefreshed;
+    }
+}
